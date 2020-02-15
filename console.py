02951e6c58c331acad06c17cd99bec388f685be3
kael1706 <cdcp-1795@hotmail.com>
2020-02-15 17:31:29 -0500

    task 6, done

--- console.py
+++ console.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+pending
+"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """pending"""
+
+    prompt = '(hbnb) '
+
+    def do_EOF(self, line):
+        """
+        ctrl+d (event)
+        """
+        print('')
+        return True
+
+    def do_quit(self, line):
+        """
+        quit (event)
+        """
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
