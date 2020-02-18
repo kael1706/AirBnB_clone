c048e1840bebdadb041f66d2e36925c32b8ed87a
kael1706 <cdcp-1795@hotmail.com>
2020-02-18 16:24:29 -0500

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -4,3 +4,15 @@ import os
 import unittest
 import models
 from models.state import State
+
+class TestState(unittest.TestCase):
+    """pending"""
+
+    def test_create(self):
+        """pending"""
+        self.s1 = State()
+
+    def test_uuid(self):
+        """know if id exist"""
+        self.assertTrue(hasattr(self.s1, 'id'))
+
