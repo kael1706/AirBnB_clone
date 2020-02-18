bb6b81a1328e15c7bef28de09d3c395e35de6e35
kael1706 <cdcp-1795@hotmail.com>
2020-02-18 16:09:45 -0500

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,3 +4,14 @@ import os
 import unittest
 import models
 from models.user import User
+
+class TestUser(unittest.TestCase):
+    """pending"""
+
+    def test_create_user(self):
+        """pending"""
+        self.user1 = User()
+        User.email = ''
+        User.password = ''
+        User.first_name = ''
+        User.last_name = ''
