076ec03bd91de746036c7f9479592edf1a1f04ab
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-02-18 19:21:11 +0000

    more fixes

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,3 +4,13 @@ import os
 import unittest
 import models
 from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """pending"""
+    def test_create_model(self):
+        """pending"""
+        self.bm1 = BaseModel()
+
+if __name__ == '__main__':
+    unittest.main()
