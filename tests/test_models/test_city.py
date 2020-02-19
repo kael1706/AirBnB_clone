db978f914ecd9f6ac243fec6861767395eb22bb8
kael <cdcp-1795@hotmail.com>
2020-02-19 05:13:04 +0000

    added city test

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -4,15 +4,119 @@ import os
 import unittest
 import models
 from models.city import City
-
+import time
+from models.base_model import BaseModel
+from datetime import datetime
 
 class TestCity(unittest.TestCase):
     """test class City"""
 
-    def test_create(self):
+    def setUp(self):
         """know if i can create a instance"""
         self.c1 = City()
+        time.sleep(1)
+        self.c2 = City()
+        City.state_id = ''
+        City.name = ''
 
     def test_uuid(self):
-        """know if id exist"""
+        """should have a id"""
         self.assertTrue(hasattr(self.c1, 'id'))
+
+    def test_type_id(self):
+        """should be a sttring"""
+        self.assertTrue(isinstance(self.c1.id, str))
+
+    def test_uniq_uuid(self):
+        """should be a unique"""
+        self.assertNotEqual(self.c1.id, self.c2.id)
+
+    def test_uuid_str(self):
+        """id should be a str"""
+        self.assertIsInstance(self.c1.id, str)
+
+    def test_user_model(self):
+        """should be a instance of model"""
+        self.assertIsInstance(self.c1, City)
+
+    def test_BaseModel(self):
+        """should be a instance of BaseModel"""
+        self.assertIsInstance(self.c1, BaseModel)
+
+    def test_created_at_exists(self):
+        """should have created_at"""
+        self.assertTrue(hasattr(self.c1, 'created_at'))
+
+    def test_created_at_datetime(self):
+        """should be a datatype"""
+        self.assertTrue(isinstance(self.c1.created_at, datetime))
+
+    def test_create_update_equal(self):
+        """should have the same date and time"""
+        self.assertEqual(self.c1.created_at, self.c1.updated_at)
+    
+    def test_created_not_equal1(self):
+        """should have diferent date and time"""
+        self.assertNotEqual(self.c1.created_at, self.c2.created_at)
+
+    def test_create_update_not_equal2(self):
+        """should have diferent date and time"""
+        self.c1.updated_at = datetime.now()
+        self.assertNotEqual(self.c1.created_at, self.c1.updated_at)
+
+    def test_updated_at_exists(self):
+        """should have updated_at"""
+        self.assertTrue(hasattr(self.c1, 'updated_at'))
+
+    def test_updated_at_datetime(self):
+        """should be a datatype"""
+        self.assertTrue(isinstance(self.c1.updated_at, datetime))
+
+    def test_class_value_attrs(self):
+        """testing value attrs"""
+        self.assertEqual(City.state_id, '')
+        self.assertEqual(City.name, '')
+
+    def test_value_attrs(self):
+        """testing value of attr"""
+        self.assertEqual(self.c1.state_id, '')
+        self.assertEqual(self.c1.name, '')
+
+    def test_input_dict(self):
+        """testing set attr as dict"""
+        attrs = {"id": "1"}
+        self.c1 = City(**attrs)
+        self.assertEqual(attrs['id'], self.c1.id)
+
+    def test_name(self):
+        """Testing first name"""
+        self.assertTrue(hasattr(self.c1, 'state_id'))
+        self.assertTrue(isinstance(self.c1.state_id, str))
+        self.assertEqual(self.c1.state_id, '')
+
+    def test_last_name(self):
+        """Testing last name"""
+        self.assertTrue(hasattr(self.c1, 'name'))
+        self.assertTrue(isinstance(self.c1.name, str))
+        self.assertEqual(self.c1.name, '')
+
+    def test_to_dict(self):
+        """ testing create and update """
+        date_frmt = "%Y-%m-%dT%H:%M:%S.%f"
+        d = self.c1.to_dict()
+        self.assertEqual(d["created_at"],
+                         self.c1.created_at.strftime(date_frmt))
+        self.assertEqual(d["updated_at"],
+                         self.c1.updated_at.strftime(date_frmt))
+        self.assertEqual(d["__class__"], 'City')
+        self.assertEqual(type(d["created_at"]), str)
+        self.assertEqual(type(d["updated_at"]), str)
+
+    def test_str(self):
+        """Testing string  of obj"""
+        output = "[{}] ({}) {}".format(
+            self.c1.__class__.__name__, self.c1.id, self.c1.__dict__)
+        self.assertEqual(str(self.c1), output)
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -25,9 +25,17 @@ class TestUser(unittest.TestCase):
         """should have a id"""
         self.assertTrue(hasattr(self.u1, 'id'))
 
+    def test_type_id(self):
+        """should be a sttring"""
+        self.assertTrue(isinstance(self.u1.id, str))
+
     def test_uniq_uuid(self):
         """should be a unique"""
         self.assertNotEqual(self.u1.id, self.u2.id)
+
+    def test_uuid_str(self):
+        """id should be a str"""
+        self.assertIsInstance(self.u1.id, str)
 
     def test_user_model(self):
         """should be a instance of model"""
@@ -37,62 +45,54 @@ class TestUser(unittest.TestCase):
         """should be a instance of BaseModel"""
         self.assertIsInstance(self.u1, BaseModel)
 
-    def test_uuid_str(self):
-        """id should b a str"""
-        self.assertIsInstance(self.u1.id, str)
-
-    def test_created_at(self):
-        """should have diferent date and time"""
-        self.assertNotEqual(self.u1.created_at, self.u2.created_at)
-
-    def test_create_update_equal(self):
-        """should have the same date and time"""
-        self.assertEqual(self.u1.created_at, self.u1.updated_at)
-
-    def test_create_update_not_equal(self):
-        """should have diferent date and time"""
-        self.u1.updated_at = datetime.now()
-        self.assertNotEqual(self.u1.created_at, self.u1.updated_at)
-
     def test_created_at_exists(self):
         """should have created_at"""
         self.assertTrue(hasattr(self.u1, 'created_at'))
-
-    def test_updated_at_exists(self):
-        """should have updated_at"""
-        self.assertTrue(hasattr(self.u1, 'updated_at'))
 
     def test_created_at_datetime(self):
         """should be a datatype"""
         self.assertTrue(isinstance(self.u1.created_at, datetime))
 
+    def test_create_update_equal(self):
+        """should have the same date and time"""
+        self.assertEqual(self.u1.created_at, self.u1.updated_at)
+    
+    def test_created_not_equal1(self):
+        """should have diferent date and time"""
+        self.assertNotEqual(self.u1.created_at, self.u2.created_at)
+
+    def test_create_update_not_equal2(self):
+        """should have diferent date and time"""
+        self.u1.updated_at = datetime.now()
+        self.assertNotEqual(self.u1.created_at, self.u1.updated_at)
+
+    def test_updated_at_exists(self):
+        """should have updated_at"""
+        self.assertTrue(hasattr(self.u1, 'updated_at'))
+
     def test_updated_at_datetime(self):
         """should be a datatype"""
         self.assertTrue(isinstance(self.u1.updated_at, datetime))
 
-    def test_class_attr_set(self):
-        """testing set in attr"""
+    def test_class_value_attrs(self):
+        """testing value attrs"""
         self.assertEqual(User.first_name, '')
         self.assertEqual(User.last_name, '')
         self.assertEqual(User.email, '')
         self.assertEqual(User.password, '')
+
+    def test_value_attrs(self):
+        """testing value of attr"""
+        self.assertEqual(self.u1.first_name, '')
+        self.assertEqual(self.u1.last_name, '')
+        self.assertEqual(self.u1.email, '')
+        self.assertEqual(self.u1.password, '')
 
     def test_input_dict(self):
         """testing set attr as dict"""
         attrs = {"id": "1"}
         self.u1 = User(**attrs)
         self.assertEqual(attrs['id'], self.u1.id)
-
-    def test_type_id(self):
-        """should be a sttring"""
-        self.assertTrue(isinstance(self.u1.id, str))
-
-    def test_my_setters(self):
-        """testing set attr"""
-        self.assertEqual(self.u1.first_name, '')
-        self.assertEqual(self.u1.last_name, '')
-        self.assertEqual(self.u1.email, '')
-        self.assertEqual(self.u1.password, '')
 
     def test_name(self):
         """Testing first name"""
@@ -118,14 +118,14 @@ class TestUser(unittest.TestCase):
         self.assertTrue(isinstance(self.u1.password, str))
         self.assertEqual(self.u1.password, '')
 
-    def test_to_dict_attr(self):
+    def test_to_dict(self):
         """ testing create and update """
-        t_f = "%Y-%m-%dT%H:%M:%S.%f"
+        date_frmt = "%Y-%m-%dT%H:%M:%S.%f"
         d = self.u1.to_dict()
         self.assertEqual(d["created_at"],
-                         self.u1.created_at.strftime(t_f))
+                         self.u1.created_at.strftime(date_frmt))
         self.assertEqual(d["updated_at"],
-                         self.u1.updated_at.strftime(t_f))
+                         self.u1.updated_at.strftime(date_frmt))
         self.assertEqual(d["__class__"], 'User')
         self.assertEqual(type(d["created_at"]), str)
         self.assertEqual(type(d["updated_at"]), str)
@@ -135,7 +135,6 @@ class TestUser(unittest.TestCase):
         output = "[{}] ({}) {}".format(
             self.u1.__class__.__name__, self.u1.id, self.u1.__dict__)
         self.assertEqual(str(self.u1), output)
-
 
 if __name__ == '__main__':
     unittest.main()
